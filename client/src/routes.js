9a71732d315965e917d4c9a331ee1f3b4fcffebd
emmanuelferdinand <emmanuelferdinand42@gmail.com>
2024-12-18 00:55:21 +0800

    Remove sign in

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,22 +1,17 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/Home';
-import Product from './pages/Product/Product';
-import Auth from './pages/Auth/Auth';
-import Profile from './pages/Profile/Profile';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Product from "./pages/Product/Product";
 import AddProduct from "./pages/AddProduct/AddProduct";
-import Cart from "./Cart";
+import Auth from "./pages/Auth/Auth";
 
-
-const AppRoutes = () => {
+const AppRoutes = ({ role }) => {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/auth" element={<Auth />} />
-            <Route path="/product" element={<Product />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/add-product" element={<AddProduct />} />
-            <Route path="/cart" element={<Cart />} />
+            <Route path="/product" element={<Product role={role} />} />
+            {role === "Seller" && <Route path="/add-product" element={<AddProduct />} />}
+
         </Routes>
     );
 };
