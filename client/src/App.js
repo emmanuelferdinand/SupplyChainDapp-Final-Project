421e0aef6f062b7fece7191f6d2412b69e2a5ce0
emmanuelferdinand <emmanuelferdinand42@gmail.com>
2024-12-18 00:55:01 +0800

    Role change

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Header from './components/Header';
-import AppRoutes from './routes';
+import React, { useState } from "react";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
+import AppRoutes from "./routes";
 
 const App = () => {
+    const [role, setRole] = useState("Buyer"); // Default role
+
+    const handleRoleChange = (newRole) => {
+        setRole(newRole);
+    };
+
     return (
         <BrowserRouter>
-            <Header />
-            <AppRoutes />
+            <Header onRoleChange={handleRoleChange} />
+            <AppRoutes role={role} />
         </BrowserRouter>
     );
 };
