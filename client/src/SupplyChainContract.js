5358aeb16b448f64587c234dc8cb2d99d0a353f9
emmanuelferdinand <emmanuelferdinand42@gmail.com>
2024-12-18 23:07:33 +0800

    ChangeContractAddress

--- client/src/SupplyChainContract.js
+++ client/src/SupplyChainContract.js
@@ -1,7 +1,7 @@
 import web3 from "./web3";
 import SupplyChain from "./link/SupplyChain.json";
 
-const contractAddress = "0xc0d76484c48ec9e0591e44098da265bea9173832"; // Replace with your contract address
+const contractAddress = "0xc0d76484c48ec9e0591e44098da265bea917383"; // Replace with your contract address
 
 let contract;
 try {
